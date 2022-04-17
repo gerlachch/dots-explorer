492ff47203fe3986ccccc9b36cc488f3f5c105e1
Christopher Gerlach <gerlachch@gmx.com>
2022-04-17 15:55:41 +0200

    use app name for config and log files

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,22 @@
 #include <backends/GlfwBackend.h>
 #include <components/MainWindow.h>
 #include <boost/asio.hpp>
+#include <fmt/format.h>
 #include <dots/Application.h>
 
 int main()
 {
+    std::string appName = "dots-imgui";
+    std::string appTitle = fmt::format("{}", appName);
+    std::string appConfig = fmt::format("{}.ini", appName);
+    std::string appLog = fmt::format("{}.log", appName);
+
     dots::Application app{ "dots-imgui" };
-    GlfwBackend backend{ 1280, 720, "dots-imgui" };
+
+    GlfwBackend backend{ 1280, 720, appTitle };
+    ImGui::GetIO().IniFilename = appConfig.data();
+    ImGui::GetIO().LogFilename = appLog.data();
+
     MainWindow mainWindow;
 
     dots::publish(DotsDescriptorRequest{});
