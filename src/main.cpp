9e23f414d73720b34057f65bf01d843eb586a016
Christopher Gerlach <gerlachch@gmx.com>
2022-04-18 15:55:36 +0200

    rename application to dots-explorer

--- src/main.cpp
+++ src/main.cpp
@@ -6,12 +6,12 @@
 
 int main(int argc, char* argv[])
 {
-    std::string appName = "dots-imgui";
+    std::string appName = "dots-explorer";
     std::string appTitle = fmt::format("{}", appName);
     std::string appConfig = fmt::format("{}.ini", appName);
     std::string appLog = fmt::format("{}.log", appName);
 
-    dots::Application app{ "dots-imgui", argc, argv };
+    dots::Application app{ "dots-explorer", argc, argv };
 
     GlfwBackend backend{ 1280, 720, appTitle };
     ImGui::GetIO().IniFilename = appConfig.data();
