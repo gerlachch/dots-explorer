e1be178b1795c0217c2f56a61dded575e7b32d08
Christopher Gerlach <gerlachch@gmx.com>
2022-04-18 15:49:24 +0200

    relay DOTS application options

--- src/main.cpp
+++ src/main.cpp
@@ -4,14 +4,14 @@
 #include <fmt/format.h>
 #include <dots/Application.h>
 
-int main()
+int main(int argc, char* argv[])
 {
     std::string appName = "dots-imgui";
     std::string appTitle = fmt::format("{}", appName);
     std::string appConfig = fmt::format("{}.ini", appName);
     std::string appLog = fmt::format("{}.log", appName);
 
-    dots::Application app{ "dots-imgui" };
+    dots::Application app{ "dots-imgui", argc, argv };
 
     GlfwBackend backend{ 1280, 720, appTitle };
     ImGui::GetIO().IniFilename = appConfig.data();
