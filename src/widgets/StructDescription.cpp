120066fdd72232d469ce38babf3a981c5a26c4a2
Christopher Gerlach <gerlachch@gmx.com>
2022-04-24 20:20:18 +0200

    show type flags in struct descriptions

--- src/widgets/StructDescription.cpp
+++ src/widgets/StructDescription.cpp
@@ -5,6 +5,46 @@ StructDescription::StructDescription(const dots::type::StructDescriptor<>& descr
 {
     m_parts.emplace_back("struct", ColorThemeActive.Keyword);
     m_parts.emplace_back(descriptor.name(), ColorThemeActive.UserType);
+
+    if (uint8_t flags = descriptor.flags(); flags > 1)
+    {
+        std::string part = "[";
+
+        if (!descriptor.cached())
+        {
+            part += "cached=false, ";
+        }
+
+        if (descriptor.cleanup())
+        {
+            part += "cleanup, ";
+        }
+
+        if (descriptor.local())
+        {
+            part += "local, ";
+        }
+
+        if (descriptor.persistent())
+        {
+            part += "persistent, ";
+        }
+
+        if (descriptor.internal())
+        {
+            part += "internal, ";
+        }
+
+        if (descriptor.substructOnly())
+        {
+            part += "substruct_only, ";
+        }
+
+        part.erase(part.size() - 2);
+        part += ']';
+
+        m_parts.emplace_back(std::move(part), ColorThemeActive.Keyword);
+    }
 }
 
 void StructDescription::render()
