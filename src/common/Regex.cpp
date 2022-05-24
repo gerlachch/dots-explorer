67fd3b0c4b6c16ac19fa7c5232e9d1149942fb66
Christopher Gerlach <gerlachch@gmx.com>
2022-05-24 18:35:55 +0200

    replace std::regex with re2 library
    For a variety of reasons, std::regex is notoriously slow in most
    implementations.
    
    While this is often not that significant for simple evaluations, it
    makes a dramatic difference for things like the trace view, where
    regular expressions have to be applied to potentially tens or hundreds
    of thousands of events.
    
    To ensure that the filtering is responsive (or "snappy"), the
    implementation must be replaced with something more performant.
    
    While there are a couple of attractive options available (particularly
    hyperscan), many of them are usually non-trivial to integrate in a
    portable way, due to various dependencies.
    
    Google's re2 library on the other hand, is a comparatively simple,
    proven and popular engine, which can easily be obtained and used via
    FetchContent_Declare. At the same time, it is still among the fastest
    solutions around (see [1]).
    
    Note that due to its design, re2 does not support lookarounds, which,
    among other things, makes it impossible to specify inverted filters.
    
    References:
    
     - [1] https://github.com/HFTrader/regex-performance

--- src/common/Regex.cpp
+++ src/common/Regex.cpp
@@ -1,14 +1,16 @@
 #include <common/Regex.h>
-#include <regex>
+#include <fmt/format.h>
 
 Regex::Regex(std::string_view regex, bool matchCase/* = true*/) :
-    m_regex{ std::make_any<std::regex>(std::string{ regex }, matchCase ? std::regex_constants::ECMAScript : std::regex_constants::ECMAScript | std::regex_constants::icase) }
+    m_regex{ std::make_unique<re2::RE2>(fmt::format("{}{}", matchCase ? "" : "(?i)", regex), RE2::Options{ RE2::Quiet })}
 {
-    /* do nothing */
+    if (!m_regex->ok())
+    {
+        throw std::runtime_error{ "invalid regex input: " + m_regex->error() };
+    };
 }
 
 bool Regex::search(std::string_view str)
 {
-    auto& regex = std::any_cast<const std::regex&>(m_regex);
-    return std::regex_search(std::string{ str }, regex);
+    return RE2::PartialMatch(str, *m_regex);
 }
--- src/common/Regex.h
+++ src/common/Regex.h
@@ -1,14 +1,21 @@
 #pragma once
-#include <any>
 #include <string_view>
+#include <memory>
+#include <re2/re2.h>
 
 struct Regex
 {
     Regex(std::string_view regex, bool matchCase = true);
+    Regex(const Regex& other) = delete;
+    Regex(Regex&& other) = default;
+    ~Regex() = default;
+
+    Regex& operator = (const Regex& rhs) = delete;
+    Regex& operator = (Regex&& rhs) = default;
 
     bool search(std::string_view str);
 
 private:
 
-    std::any m_regex;
+    std::unique_ptr<RE2> m_regex;
 };
