9e4f432c431ce21e64130e87864352c2ee55ca61
Christopher Gerlach <gerlachch@gmx.com>
2023-02-21 21:52:36 +0100

    prepare release v1.0.0

--- src/common/Version.h
+++ src/common/Version.h
@@ -5,7 +5,7 @@
 
 struct Version
 {
-    static constexpr std::string_view CurrentVersion = "v0.9.1";
+    static constexpr std::string_view CurrentVersion = "v1.0.0";
 
     static std::future<GitHubReleaseInfo> GetReleaseInfo(std::string_view release = "latest");
 };
